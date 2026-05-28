858423ee13d98f981634f823dd7d019b401acd53
GentlePanda88 <arifsuffian.azlan@gmail.com>
2026-05-28 14:21:54 +0800

    Initial draft for chatbot webpage

--- week_3/frontend/src/main.py
+++ week_3/frontend/src/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-
+from dotenv import load_dotenv
+import os
+ 
+load_dotenv()
+ 
 app = FastAPI()
-
+ 
 templates = Jinja2Templates(directory="src/templates")
-
+ 
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse(
         "index.html",
-        {"request": request}
+        {
+            "request": request,
+            "backend_url": os.getenv("BACKEND_URL", "http://127.0.0.1:8001")
+        }
     )
